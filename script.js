bd325292eec0eb450b1a6d90992f04186212ef52
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-19 16:02:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function volume_sphere() {
-    //Write your code here
-	let r = document.getElementById("radius").value;
-  let vol = (4/3) * Math.PI * Math.pow(r,3);
-	document.getElementById("volume").value = vol;
-} 
+  var radius = parseFloat(document.getElementById("radius").value);
+  var volumeField = document.getElementById("volume");
 
+  if (isNaN(radius) || radius <= 0) {
+    volumeField.value = "Invalid radius";
+  } else {
+    var volume = (4/3) * Math.PI * Math.pow(radius, 3);
+    volumeField.value = volume.toFixed(2);
+  }
+}
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
