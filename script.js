8099a76a5bda45da9cffb1bc157bf8f6170fe766
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-19 15:49:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function volume_sphere() {
     //Write your code here
-  
+	let r = document.getElementById("radius");
+  let vol = 4/3*22/7*(r**2);
+	document.getElementById("volume").innerHTML = vol;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
