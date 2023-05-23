e39931b4272a220b016227ee1d5f49ee39ba1f81
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-23 19:43:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function volume_sphere() {
-  let radius = document.getElementById("radius").value;
-  let volumeField = document.getElementById("volume");
-
-  if (isNaN(radius) || radius <= 0) {
-    volumeField.value = "Invalid radius";
-  } else {
-    let volume = (4/3) * Math.PI * Math.pow(radius, 3);
-    volumeField.value = volume.toFixed(2);
-	  // volumeField.innerText = volumeField.value;
+    let radius = document.getElementById("radius").value;
+    let volumeField = document.getElementById("volume");
+  
+    if (isNaN(radius) || radius <= 0) {
+      volumeField.value = "Invalid radius";
+    } else {
+      let volume = (4/3) * Math.PI * Math.pow(radius, 3);
+      volumeField.value = volume.toFixed(2);
+        // volumeField.innerText = volumeField.value;
+        
+    }
+    volumeField.value.style.display;
+      //button.addEventListener("click", volumeField.value.style.display)
   }
+//   window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
 let button = document.getElementById("submit");
-	button.addEventListener("click", volumeField.value.style.display)
-}
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+  button.addEventListener("click", volume_sphere)
+
+  
